d789aef6360e1dc632782caa7f49480b6ba4493d
Ogrebalschick <ilya.ya.2011@bk.ru>
2021-10-25 10:55:07 +0600

    копия сайта https://flsindustries.ru/m мобильная версия

---
+++ script/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			document.location.href = "http://flsindustriescopy/form.html";
+		});
+		return false;
+	});
+
+});
+$(document).ready(function(){
+	$('.block6__images').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		speed: 200
+	  });
+			  
+})
+
+$(document).ready(function(){
+	$('.block7__images').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		speed: 600,
+		autoplay: true,
+  autoplaySpeed: 5000,
+	  });
+			  
+})
+
+$(document).ready(function(){
+	//Скрыть PopUp при загрузке страницы    
+	PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+	$("#popup1").show();
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+	$("#popup1").hide();
+}
+
+$(document).ready(function(){
+	//Скрыть PopUp при загрузке страницы    
+	PopUpHide2();
+});
+//Функция отображения PopUp
+function PopUpShow2(){
+	$("#popup2").show();
+}
+//Функция скрытия PopUp
+function PopUpHide2(){
+	$("#popup2").hide();
+}
